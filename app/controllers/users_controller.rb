633e7af2763ab98cd5ccae21c2d6c3d9baaa69db
Eric Johnson <johnson.n.eric@gmail.com>
2020-07-16 12:24:32 -0500

    first

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+    skip_before_action :logged_in?, only: [:create, :show]
+
+    def show
+        @user = User.find(params[:id])
+        render json: @user 
+    end
+    
+    def create
+        @user = User.new(user_params)
+        if @user.valid?
+            @user.save
+            render json: { user: @user }, status: :created
+        else
+            render json: { error: 'failed to create user'}, status: :not_acceptable
+        end
+    end
+    
+    def update
+         @user = User.find(params[:id])
+         if @user.update(user_params)
+            render json: @user
+        else 
+            render json: { error: 'failed to update user'}, status: :not_acceptable
+         end
+     end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :first_name, :last_name)
+    end
+
+end
